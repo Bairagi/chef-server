a735747a7b8a423ef0af2690e94bc5452462f677
Nileshb <nileshbairagi@gmail.com>
2014-08-21 18:31:29 +0530

    Nilesh | fixed chef-server install in lxc

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,33 +8,41 @@ execute "set_hostname" do
   not_if "hostname | grep #{node['chef-server']['fqdn']}"
 end
 
-script "set_fqdn" do
+file "/etc/hostname" do
+  mode "0644"
+  content node['chef-server']['fqdn']
+end
+
+script "add_hostentry" do
   interpreter "bash"
   user "root"
   code <<-EOH
-    hostname #{node['chef-server']['fqdn']}
-    echo "#{node['chef-server']['fqdn']}" >> /etc/hostname
     echo "127.0.0.1 #{node['chef-server']['fqdn']}" >> /etc/hosts    
   EOH
-  not_if "hostname -f | grep #{node['chef-server']['fqdn']}"
+  not_if "grep -ri #{node['chef-server']['fqdn']} /etc/hosts"
 end
 
-execute "get_chef-server" do
+execute "downloading chef-server" do
   cwd "/opt"
   command "wget #{node['chef-server']['url']}/#{node['chef-server']['package_file']}"
   not_if { File.exists?("/opt/#{node['chef-server']['package_file']}") }
 end
 
-execute "install_chef-server" do
+execute "installing chef-server" do
   cwd "/opt"
   command "dpkg -i #{node['chef-server']['package_file']}"
   notifies :run, 'execute[reconfigure-chef-server]', :immediately
 end
 
+# chef-server installation hack for lxc
+file "/opt/chef-server/embedded/cookbooks/chef-server/templates/default/postgresql.conf.erb" do
+  mode "0644"
+  source "postgresql.conf"
+end
 
 # reconfigure the installation
 execute 'reconfigure-chef-server' do
-  command 'chef-server-ctl reconfigure'
   action :nothing
+  command 'chef-server-ctl reconfigure'
 end
 
